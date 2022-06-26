6dd77ea1b9cfd4048ce706681edda18b2e8d9cf1
Frances Naomi Alido <55601810+namiSunflower@users.noreply.github.com>
2022-06-26 11:19:19 +0800

    reset to previous app

--- server.js
+++ server.js
@@ -9,19 +9,19 @@ const cors = cors = require('cors');
 const port = process.env.PORT || 3000;
 const app = express();
 
+connectDB();
 
-app.use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: false }))
+app
+  .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
-  }).use(cors())
+  })
   .use('/', require('./routes'))
   .use('/petHotel', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-  connectDB();
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB');
     app.listen(port, () => console.log(`Server running on port ${port}`));});
