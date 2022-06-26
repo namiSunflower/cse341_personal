9ea0df18f73671de09579e8bdc81fd2cdcfc2028
Frances Naomi Alido <55601810+namiSunflower@users.noreply.github.com>
2022-06-26 11:17:39 +0800

    moved connectDb

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const cors = cors = require('cors');
 const port = process.env.PORT || 3000;
 const app = express();
 
-connectDB();
 
 app.use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: false }))
@@ -22,6 +21,7 @@ app.use(bodyParser.json())
   .use('/', require('./routes'))
   .use('/petHotel', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
+  connectDB();
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB');
     app.listen(port, () => console.log(`Server running on port ${port}`));});
