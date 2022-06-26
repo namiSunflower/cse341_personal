3336d6f38ab168ff2a2c27316eceaf1a4fb09ab4
Frances Naomi Alido <55601810+namiSunflower@users.noreply.github.com>
2022-06-26 11:08:53 +0800

    fixing error

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const connectDB = require('./db/connect')
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger_output.json')
 const mongoose = require('mongoose');
-const cors = cors = require('cors');
+
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -19,7 +19,7 @@ app
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
-  }).use(cors())
+  })
   .use('/', require('./routes'))
   .use('/petHotel', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
